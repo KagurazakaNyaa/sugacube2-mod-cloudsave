620e5ab439a226ed59bda57f86c22a6e6267a701
KagurazakaNyaa <dev@kagurazakanyaa.com>
2025-02-24 14:52:04 +0800

    change mod builder

--- packmod.sh
+++ packmod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 
-docker build . -t modpacker:latest -f modpack.dockerfile
+BUILDER_IMAGE=${BUILDER_IMAGE:-ghcr.io/lyoko-jeremie/sugarcube-2-modloader-inserttools:latest}
 
-docker run --rm -u "$(id -u):$(id -g)" -v "$(pwd)/src":/src modpacker:latest
+docker run --rm -u "$(id -u):$(id -g)" -v "$(pwd)/src":/src "${BUILDER_IMAGE}"
